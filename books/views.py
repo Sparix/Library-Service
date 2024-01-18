4703c94446602fee78492268cd403f58ee2a3c82
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-18 18:12:01 +0200

    create viewset for model book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Books
+from books.serializers import BookSerializer, BookSerializerList
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Books.objects.prefetch_related(
+        "genre"
+    ).select_related(
+        "author"
+    )
+    serializer_class = BookSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookSerializerList
+        return BookSerializer
