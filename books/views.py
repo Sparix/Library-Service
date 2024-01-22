90c9fb228dbda83f4b3160cc03026e35b7ee9498
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-22 17:17:11 +0200

    Add permisson for book, non-admin user can create or read

--- books/views.py
+++ books/views.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets
 
 from books.models import Books
+from books.permissions import IsAdminOrReadOrCreate
 from books.serializers import BookSerializer, BookSerializerList
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.prefetch_related("genre").select_related("author")
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOrCreate, )
 
     def get_serializer_class(self):
         if self.action == "list":
