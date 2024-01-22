a2a31d7fa9343fea5be9fef2519b44678838914c
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-22 09:34:10 +0200

    create endpoint for user

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ProfileUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), "register"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("profile/", ProfileUserView.as_view(), name="profile"),
 ]
 
 app_name = "user"
