f06517a04bdd117dad6047f64c55858f12fa1609
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-23 16:01:31 +0200

    create serializer for model Borrowing

---
+++ borrowing/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrowing_date", "expected_return_date", "actual_return_date", "book", "user")
