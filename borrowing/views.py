6e6fae0f192c776fa95ff321084e23ec14236e44
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-23 16:22:15 +0200

    Implement viewset for 'Borrowing' model, incorporating distinct serializers for various actions.

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related(
+        "book__author", "user"
+    ).prefetch_related("book__genre")
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BorrowingListSerializer
+
+        return self.serializer_class
