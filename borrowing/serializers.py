4def7b4edd32ee7b91f08d273652b96afd90b0da
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-23 16:02:19 +0200

    create BorrowingListSerializer for action list and retrieve

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,9 +1,22 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from books.serializers import BookSerializerList
 from borrowing.models import Borrowing
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email",)
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("id", "borrowing_date", "expected_return_date", "actual_return_date", "book", "user")
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = BookSerializerList(read_only=True)
+    user = UserSerializer(read_only=True)
