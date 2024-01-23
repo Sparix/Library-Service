11ffa4223b46f5a20783ea08ca6f796e4142e05f
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-23 16:23:43 +0200

    fix bag whith endpoint borrowing, and add endpoint for BorrowingViewSet

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,4 +1,12 @@
-urlpatterns = [
+from django.urls import include, path
+from rest_framework import routers
 
+from borrowing.views import BorrowingViewSet
+
+router = routers.DefaultRouter()
+router.register("borrowing", BorrowingViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
 ]
 app_name = "borrowing"
