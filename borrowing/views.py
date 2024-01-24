7c1a25035918d4137e5cb9341bfefe7167e941b0
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-24 15:40:16 +0200

    create custom action for return book

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,16 @@
-from rest_framework import viewsets
+import datetime
+
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingRetrieveSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingRetrieveSerializer, BorrowingReturnSerializer
+)
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -39,3 +47,21 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["put"])
+    def borrowing_return(self, request, pk=None):
+        borrowing = self.get_object()
+        serializer = BorrowingReturnSerializer(borrowing, data=request.data)
+        if borrowing.actual_return_date is None:
+            if serializer.is_valid():
+                serializer.update(borrowing, serializer.validated_data)
+
+                return Response({
+                    "actual_return_date": "You have returned the book"
+                }, status=status.HTTP_200_OK)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"actual_return_date": "You have already returned this book"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
