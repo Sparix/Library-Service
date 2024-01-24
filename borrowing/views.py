d047ff8ae2f55e701690ce340a6617c1e66ed16e
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-24 11:35:54 +0200

    add filtering so that an unauthorised user sees only their borrowing, and add permission

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
@@ -9,12 +10,20 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         "book__author", "user"
     ).prefetch_related("book__genre")
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return BorrowingListSerializer
 
         return self.serializer_class
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
