64a4f43345ce51840af65e29d8080bdf8699440c
Olexandr Hontarenko <sanya221138@gmail.com>
2024-01-26 11:31:39 +0200

    add test for admin, with update, create, and delete book

--- books/tests/test_book_api.py
+++ books/tests/test_book_api.py
@@ -152,3 +152,75 @@ class AuthenticatedBookApiTest(UnauthenticatedBookApiTest):
         res = self.client.delete(url)
 
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class IsAdminUserBookApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.user = get_user_model().objects.create_user(
+            email="admin@admin.com",
+            password="test12345",
+            is_staff=True,
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_auth_required(self):
+        res = self.client.get(BOOK_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_list_book(self):
+        sample_book()
+        authors = author_create()
+        sample_book(author=authors)
+        genre = Genres.objects.create(name="Novel")
+        genre1 = Genres.objects.create(name="TestGenre")
+        book_with_genre = sample_book()
+        book_with_genre.genre.add(genre, genre1)
+        res = self.client.get(BOOK_URL)
+        books = Books.objects.all()
+
+        serializer = BookSerializerList(books, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_book(self):
+        authors = author_create()
+        genre = Genres.objects.create(name="Novel")
+        genre1 = Genres.objects.create(name="TestGenre")
+        payload = {
+            "title": "test_book",
+            "author": authors.id,
+            "genre": [genre1.id, genre.id],
+            "inventory": 1,
+            "daily_fee": 1.9,
+            "cover": "soft"
+        }
+
+        res = self.client.post(BOOK_URL, payload)
+        book = Books.objects.get(id=res.data["id"])
+        genres = book.genre.all()
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(genres.count(), 2)
+        self.assertIn(genre, genres)
+        self.assertIn(genre1, genres)
+
+    def test_detail_book_update(self):
+        book = sample_book()
+        genre = Genres.objects.create(name="Novel")
+        url = detail_book(book.id)
+        res = self.client.patch(
+            url,
+            book.genre.add(genre)
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_detail_book_delete(self):
+        book = sample_book()
+        url = detail_book(book.id)
+
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
